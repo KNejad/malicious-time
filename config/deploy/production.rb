8dc639a2180ec5bb0b6be964b798c6043b0affe3
Keeyan Nejad <keeyan@keeyan.xyz>
2019-10-14 01:04:24 +0100

    Update deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,7 @@
-server "time.keeyan.xyz", user: "deploy", roles: %w{web app db}
+if ENV["DEPLOY_ADDRESS"]
+  server ENV["DEPLOY_ADDRESS"], user: "deploy", roles: %w{web app db}
+else
+  server "time.keeyan.xyz", user: "deploy", roles: %w{web app db}
+end
 
 set :application, "time.keeyan.xyz"
