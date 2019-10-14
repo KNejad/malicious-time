a13b76818ced3079bee9519b3c0f6ff298e40eaa
Keeyan Nejad <keeyan@keeyan.xyz>
2019-10-14 15:15:51 +0100

    Use utc time

--- app/controllers/current_time_controller.rb
+++ app/controllers/current_time_controller.rb
@@ -1,6 +1,6 @@
 class CurrentTimeController < ApplicationController
   def index
-    now = DateTime.now
-    render plain: "datetime(#{now.year}, #{now.month}, #{now.day}, #{now.hour}, #{now.minute})"
+    now = DateTime.now.utc
+    render plain: "datetime(#{now.year}, #{now.month}, #{now.day}, #{now.hour}, #{now.min})"
   end
 end
